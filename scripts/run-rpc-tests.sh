dd3801da51250f16b7c9d9653f2ce24abae3bc54
romsters <petriv.roma@gmail.com>
2025-05-22 14:16:26 +0300

    fix: use envs in rpc tests script

--- scripts/run-rpc-tests.sh
+++ scripts/run-rpc-tests.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 cd lib/rpc-tests
 
 # defaults — you can override by exporting before calling this script:
-EXEC_TESTS_EXEC_TEST_PATH=${EXEC_TEST_PATH:-./tests/cancun/eip5656_mcopy/test_mcopy.py}
+RPC_TESTS_EXEC_TEST_PATH=${EXEC_TEST_PATH:-rpc/eth}
 
 echo "▶ Running rpc-tests against http://localhost:3050"
 
-npm run test-gobi rpc/eth
+npm run test-gobi "${RPC_TESTS_EXEC_TEST_PATH}"
