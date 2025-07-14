a753546e3ceb428abe5dff2291c4bafdee989ecf
Anton Baliasnikov <aba@matterlabs.dev>
2025-07-14 13:42:58 +0100

    chore: fix url

--- scripts/run-exec-tests.sh
+++ scripts/run-exec-tests.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+set -x
+
 cd lib/execution-spec-tests
 
 # defaults — you can override by exporting before calling this script:
-EXEC_TESTS_L2_RPC_URL=${L2_RPC_URL:-http://localhost:3050}
+EXEC_TESTS_L2_RPC_URL=${L2_RPC_URL:-http://localhost:3053}
 EXEC_TESTS_CHAIN_ID=${CHAIN_ID:-271}
 EXEC_TESTS_PRIVATE_KEY=${PRIVATE_KEY:-0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110}
-EXEC_TESTS_EXEC_TEST_PATH=${EXEC_TEST_PATH:-./tests/cancun/eip5656_mcopy/test_mcopy.py}
+EXEC_TESTS_EXEC_TEST_PATH=${EXEC_TEST_PATH:-./tests/cancun/eip1153_tstore}
 
 echo "▶ Running execution-spec-tests (Cancun fork) against ${EXEC_TESTS_L2_RPC_URL}"
 
@@ -15,8 +17,9 @@ uv run execute remote \
   --fork Cancun \
   --rpc-endpoint "${EXEC_TESTS_L2_RPC_URL}" \
   --rpc-chain-id "${EXEC_TESTS_CHAIN_ID}" \
-  --rpc-seed-key "${EXEC_TESTS_PRIVATE_KEY}" \
+  --rpc-seed-key${EXEC_TESTS_PRIVATE_KEY}" \
   --junitxml="junit-report.xml" \
   --json-report \
+  -m "not slow" \
   -n auto \
   "${EXEC_TESTS_EXEC_TEST_PATH}"
